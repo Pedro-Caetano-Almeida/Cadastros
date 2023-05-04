55617ed23aae0045384ed7d276af264de2d99d6b
PEDRO CAETANO <pedro.caetano.acm@gmail.com>
2023-05-03 22:08:33 -0300

    first commit

---
+++ CadastroCliente/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from CadastroCliente.models import Cliente, Profissao, Telefone, Interesse
+# Register your models here.
+
+class Telefones(admin.StackedInline):
+    model = Telefone
+    extra = 1
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'cidade']
+    list_display_links = ['id', 'nome']
+    list_filter = ['bairro', 'cidade', 'ativo']
+    search_fields = ['nome']
+    inlines = [Telefones]
+    
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Profissao)
+admin.site.register(Telefone)
+admin.site.register(Interesse)
