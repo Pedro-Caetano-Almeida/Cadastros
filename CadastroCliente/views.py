8ae9dc3a2485128e07e8adb745e53faaa87326bb
PEDRO CAETANO <pedro.caetano.acm@gmail.com>
2023-05-08 22:30:47 -0300

    Subindo para o site

--- CadastroCliente/views.py
+++ CadastroCliente/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    meu_nome = "Caetano"
+    lista_frutas = ['Laranja','Maçã','Banana']
+    context = {
+        'nome': meu_nome,
+        'frutas': lista_frutas
+    }
+    return render(request, 'index.html', context)
