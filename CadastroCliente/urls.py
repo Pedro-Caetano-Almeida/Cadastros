a0e93596364d477559d54797f788e32698b5afe6
PEDRO CAETANO <pedro.caetano.acm@gmail.com>
2023-05-10 22:31:42 -0300

    Alterações

--- CadastroCliente/urls.py
+++ CadastroCliente/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from CadastroCliente import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.index, name = 'index'), #o name aqui é o nome que demos para esse path
+    path('Clientes', views.listar_clientes, name='clientes'),
+    path('Profissões', views.profissoes, name = 'profissoes'),
+    path('cliente/<int:id>', views.detalhar_cliente, name = 'detalhar')
 ]
--- CadastroCliente/views.py
+++ CadastroCliente/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from CadastroCliente.models import Cliente, Profissao
 
 # Create your views here.
 
@@ -10,3 +11,27 @@ def index(request):
         'frutas': lista_frutas
     }
     return render(request, 'index.html', context)
+
+def listar_clientes(request):
+# busca todos os clientes cadastrados na tabela (admin)
+    lista_clientes = Cliente.objects.all()
+# o dicionário (variável) context é que vai mandar pro template    
+    context = {
+        "clientes": lista_clientes,
+    }
+    return render (request, 'lista_clientes.html', context)
+
+def profissoes(request):
+    profissoes = Profissao.objects.all()
+    context = {
+        "profissoes": profissoes
+    }
+    return render (request, 'profissoes.html', context)
+
+def detalhar_cliente(request, id):
+    #buscando no banco de dados o cliente pelo id
+    cliente = Cliente.objects.get(id = id)
+    context = {
+        "cliente": cliente
+    }
+    return render (request, "cliente_detalhes.html", context)
